ab97c62b3177db26f36036d117a9ec128e9f305f
schujungle <12116914+schujungle@user.noreply.gitee.com>
2023-01-12 23:11:25 +0800

    2023-1-12

--- myjs/main.js
+++ myjs/main.js
@@ -7,5 +7,28 @@ function web_trans(id, href){
 
 function set_src(id, src){
     //设置src
-    $(id).attr("src", src);
+    $(id).attr('src', src);
+}
+
+function remove_pop(id){
+    // 移除提示框的函数
+    // data-bs-toggle="popover" title="提示" data-bs-content="内容"
+    if(typeof($(id).attr('data-bs-toggle'))=="undefined"){
+        // console.log('pass');
+    }
+    else{
+        $(id).removeAttr('data-bs-toggle');
+        $(id).removeAttr('title');
+        $(id).removeAttr('data-bs-content');
+        // console.log('del');
+        popoverTriggerList = null;
+        popoverList = null;
+    }
+}
+
+function remove_class(id, cla){
+    // 如有class，去除它
+    if ($(id).hasClass(cla)){
+        $(id).removeClass(cla);
+    }
 }
